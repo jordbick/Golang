5712ef976af41c055600f1be639509c5b5f38265
Jordan <jordanbickerdyke@hotmail.co.uk>
2022-08-08 16:32:57 +0100

    Updated imports

--- inventory-service/main.go
+++ inventory-service/main.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/pluralsight/webservices/database"
-	"github.com/pluralsight/webservices/product"
+	"github.com/jordbick/Golang/inventory-service/database"
+	"github.com/jordbick/Golang/inventory-service/product"
 
 	// use underscore _ because we're not going to referencing the driver explicitly, just importing it for its side effects
 	// and tin this case because we need the driver in order for the Go SQL package to work with our database
